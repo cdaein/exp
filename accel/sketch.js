6845b50885cd39398484061bd4db14cda5834c01
Dae In Chung <Dane@NMB.local>
2018-07-19 09:34:51 -0400

    uploaded accelerometer test

---
+++ accel/sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+  var myCanvas = createCanvas(windowWidth, windowHeight);
+	myCanvas.parent('p5container');
+}
+
+function draw() {
+  // put drawing code here
+	background(123);
+}
