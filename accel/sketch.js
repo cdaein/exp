cfd6005cfd846f237da290fa65a29fee80a2e326
Dae In Chung <Dane@NMB.local>
2019-06-02 19:03:11 -0400

    accel

--- accel/sketch.js
+++ accel/sketch.js
@@ -8,38 +8,29 @@ var ypos;
 var diam;
 
 function setup() {
-  var myCanvas = createCanvas(windowWidth, windowHeight);
+	var myCanvas = createCanvas(windowWidth, windowHeight);
 	myCanvas.parent('p5container');
-	
-	xpos = width/2;
-	ypos = height/2;
+
+	xpos = width / 2;
+	ypos = height / 2;
 	diam = 100;
 }
 
 function draw() {
 	background(123);
-	
+
 	textSize(48);
 	fill(255, 255, 0);
-	text("x:"+x, 20, 40);
-	text("y:"+y, 20, 80);
-	text("z:"+z, 20, 120);
-	
-	xpos += x;
-	ypos += y;
-//	diam += z;
+	text("x:" + x, 20, 40);
+	text("y:" + y, 20, 80);
+	text("z:" + z, 20, 120);
+
+	xpos += rotationX;
+	ypos += rotationY;
+	//	diam += z;
 	if (diam > 200) diam = 200;
-	
+
 	fill(0);
 	stroke(0);
 	ellipse(xpos, ypos, diam, diam);
 }
-
-// accelerometer Data
-window.addEventListener('devicemotion', function(e) 
-{
-  // get accelerometer values
-  x = parseInt(e.accelerationIncludingGravity.x);
-  y = parseInt(e.accelerationIncludingGravity.y);
-  z = parseInt(e.accelerationIncludingGravity.z); 
-});
