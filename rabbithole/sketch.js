fa7ee6fd12485c5516e29c8243f2314bc6841d58
Dae In Chung <Dane@NMB.local>
2019-12-19 22:30:03 -0500

    updated rabbithole

--- rabbithole/sketch.js
+++ rabbithole/sketch.js
@@ -1,24 +1,336 @@
-var canvas,ctx,ratio,numShapes=6,shps=[],loc,tloc,w,h,msg="straightdowntherabbithole",str1="down, down, down. would the fall never come to an end!",str2="down, down, down. there was nothing else to do, so alice soon began talking again.",bgCol,boxCol,boxCols=[],txtCol,boxShaCol,txtShaCol,speed,totalFrames;
-function setup(){ratio=.625;var a=calcDimensionFromRatio(ratio);canvas=createCanvas(a.x,a.y);canvas.position((windowWidth-canvas.width)/2,(windowHeight-canvas.height)/2);noSmooth();w=width;h=height;colorMode(HSB,360,100,100,100);ctx=canvas.drawingContext;for(a=0;a<numShapes;a++)shps[a]=new Compound;textFont("moderno-fb");textStyle(ITALIC);textAlign(LEFT,TOP);noStroke();loc=createVector(w/2,h/2);tloc=loc.copy();vel=createVector();acc=createVector();bgCol=color(210,100,70);boxCol=color(0,0,100);boxCols[0]=
-color(0,0,0);boxCols[1]=color(355,70,95);boxCols[2]=color(120,50,95);boxCols[3]=color(0,0,92);boxCols[4]=color(60,90,95);boxCols[5]=color(0,0,100);txtCol=color(0,0,0);boxShaCol=color(0,0,0,70);txtShaCol=color(0,0,0,30);totalFrames=360}
-function draw(){background(bgCol);noStroke();fill(50,30,40);for(var a=0,c=0;c<h;c+=h/20){for(var b=0;b<w;b+=w/10)0==a%2?ellipse(b,c,20,20):ellipse(b+w/20,c,20,20);a++}speed=frameCount/totalFrames*TWO_PI;mouseIsPressed?(tloc.x=mouseX,tloc.y=mouseY):(tloc.x=w/2+constrain(map(rotationY,-30,30,-w/2,w/2),-w/2,w/2),tloc.y=h/2+constrain(map(rotationX,-30,30,-h/2,h/2),-h/2,h/2));loc.x=lerp(loc.x,tloc.x,.1);loc.y=lerp(loc.y,tloc.y,.1);ctx.shadowColor=txtShaCol;ctx.shadowBlur=.02*w;ctx.shadowOffsetX=.5*(w/
-2-loc.x);ctx.shadowOffsetY=.5*(h/2-loc.y);push();translate(w/2,h/2);fill(txtCol);textCircle(.09*h,.09*h,.05*w);pop();a=w-.125*w;c=h-.125*w;for(b=numShapes-1;0<=b;b--){var d=lerp(w/2,loc.x,b/numShapes),e=lerp(h/2,loc.y,b/numShapes),f=shps[b];f.setContourRect(d,e,a-b*w*.13,c-b*h*.13);ctx.shadowColor=boxShaCol;ctx.shadowBlur=.1*w;ctx.shadowOffsetX=0;ctx.shadowOffsetY=0;fill(90+20*b,20*b,100-6*b);f.display();ctx.shadowColor=txtShaCol;ctx.shadowBlur=.02*w;ctx.shadowOffsetX=.35*(w/2-loc.x);ctx.shadowOffsetY=
-.35*(h/2-loc.y);fill(txtCol);push();translate(w/2,h/2);fill(txtCol);1==b?textCircle(.36*h,.36*h,.16*w):3==b&&textCircle(.2*h,.2*h,.1*w);pop()}ctx.shadowColor="transparent";textSize(.033*w);textAlign(CENTER,TOP);textStyle(ITALIC);fill(50,20,70);a=.013*w;c=.013*w;push();translate(.5*w,c);text(str1,0,0);pop();push();translate(w-a,.5*h);rotate(PI/2);text(str2,0,0);pop();push();translate(.5*w,h-c);rotate(PI);text(str1,0,0);pop();push();translate(c,.5*h);rotate(-PI/2);text(str2,0,0);pop();stroke(50,20,
-70);line(.15*w,.032*h,w-.15*w,.032*h);line(.952*w,.18*h,.952*w,h-.18*h);line(.15*w,h-.032*h,w-.15*w,h-.032*h);line(w-.952*w,.18*h,w-.952*w,h-.18*h);textStyle(NORMAL);textSize(.1*w);textAlign(CENTER,CENTER);fill(50,50,60);text("\u2666",.06*w,.06*h);text("\u2665",w-.06*w,h-.04*h);text("\u2663",w-.06*w,.06*h);text("\u2660",.06*w,h-.04*h)}
-function textCircle(a,c,b){textAlign(CENTER,CENTER);textStyle(ITALIC);for(var d=0;d<msg.length;d++){var e=.7*cos(d*TWO_PI/msg.length-speed)*a,f=1.2*sin(d*TWO_PI/msg.length-speed)*c;push();translate(e,f);e=atan2(f,e);rotate(PI/2+e);textSize(b);text(msg[(d+floor(b))%msg.length],0,0);pop()}}function calcDimensionFromRatio(a){var c=windowWidth/windowHeight,b=createVector();c>a?(b.y=windowHeight,b.x=b.y*a):(b.x=windowWidth,b.y=1*b.x/a);return createVector(b.x,b.y)}
-function windowResized(){var a=calcDimensionFromRatio(ratio);resizeCanvas(a.x,a.y);canvas.position((windowWidth-canvas.width)/2,(windowHeight-canvas.height)/2);w=width;h=height}var Compound=function(){this.ct=createVector();this.ctrH=this.ctrW=0};Compound.prototype.setContourRect=function(a,c,b,d){this.ct.x=a;this.ct.y=c;this.ctrW=b;this.ctrH=d};
-Compound.prototype.display=function(){var a=.1*this.ctrW,c=.1*this.ctrW;beginShape();vertex(0,0);vertex(width,0);vertex(width,height);vertex(0,height);beginContour();vertex(this.ct.x-this.ctrW/2,this.ct.y);bezierVertex(this.ct.x-this.ctrW/2,this.ct.y+this.ctrH/2-c,this.ct.x-this.ctrW/2,this.ct.y+this.ctrH/2-c,this.ct.x-this.ctrW/2+a,this.ct.y+this.ctrH/2-c);bezierVertex(this.ct.x-this.ctrW/2+a,this.ct.y+this.ctrH/2,this.ct.x-this.ctrW/2+a,this.ct.y+this.ctrH/2,this.ct.x,this.ct.y+this.ctrH/2);bezierVertex(this.ct.x+
-this.ctrW/2-a,this.ct.y+this.ctrH/2,this.ct.x+this.ctrW/2-a,this.ct.y+this.ctrH/2,this.ct.x+this.ctrW/2-a,this.ct.y+this.ctrH/2-c);bezierVertex(this.ct.x+this.ctrW/2,this.ct.y+this.ctrH/2-c,this.ct.x+this.ctrW/2,this.ct.y+this.ctrH/2-c,this.ct.x+this.ctrW/2,this.ct.y);bezierVertex(this.ct.x+this.ctrW/2,this.ct.y-this.ctrH/2+c,this.ct.x+this.ctrW/2,this.ct.y-this.ctrH/2+c,this.ct.x+this.ctrW/2-a,this.ct.y-this.ctrH/2+c);bezierVertex(this.ct.x+this.ctrW/2-a,this.ct.y-this.ctrH/2,this.ct.x+this.ctrW/
-2-a,this.ct.y-this.ctrH/2,this.ct.x,this.ct.y-this.ctrH/2);bezierVertex(this.ct.x-this.ctrW/2+a,this.ct.y-this.ctrH/2,this.ct.x-this.ctrW/2+a,this.ct.y-this.ctrH/2,this.ct.x-this.ctrW/2+a,this.ct.y-this.ctrH/2+c);bezierVertex(this.ct.x-this.ctrW/2,this.ct.y-this.ctrH/2+c,this.ct.x-this.ctrW/2,this.ct.y-this.ctrH/2+c,this.ct.x-this.ctrW/2,this.ct.y);endContour();endShape()};
+let canvas;
+let ctx;
+let ratio;
 
-// disable elastic bounce effect on mobile browser
-function preventDefault(e){
-    e.preventDefault();
+const numShapes = 6;
+let shps = [];
+
+let loc;
+let tloc;
+
+let w, h; // substitute width and height
+
+const msg = "straightdowntherabbithole";
+const str1 = "down, down, down. would the fall never come to an end!";
+const str2 = "down, down, down. there was nothing else to do, so alice soon began talking again.";
+
+let bgCol;
+let boxCol;
+let boxCols = [];
+let txtCol;
+let boxShaCol;
+let txtShaCol;
+
+let speed;
+let totalFrames;
+
+
+function setup() {
+	ratio = 5 / 8;
+	const dim = calcDimensionFromRatio(ratio);
+	// 1080 x 1920 = 540 x 960 = 390 x 640
+	canvas = createCanvas(dim.x, dim.y);
+	canvas.position((windowWidth - canvas.width) / 2, (windowHeight - canvas.height) / 2);
+	noSmooth();
+	w = width;
+	h = height;
+	colorMode(HSB, 360, 100, 100, 100);
+
+	ctx = canvas.drawingContext;
+
+	for (let i = 0; i < numShapes; i++) {
+		shps[i] = new Compound();
+	}
+
+	//	textFont('moderno - fb ');
+	//	textFont('serif');
+	textFont('moderno-fb');
+	textStyle(ITALIC);
+	textAlign(LEFT, TOP);
+	noStroke();
+
+	loc = createVector(w / 2, h / 2);
+	tloc = loc.copy();
+	vel = createVector();
+	acc = createVector();
+
+	bgCol = color(210, 100, 70);
+	boxCol = color(0, 0, 100);
+	boxCols[0] = color(0, 0, 0);
+	//	boxCols[1] = color(310, 70, 100);
+	//	boxCols[2] = color(120, 65, 95);
+	//	boxCols[3] = color(0, 0, 100);
+	//	boxCols[4] = color(310, 50, 100);
+	boxCols[1] = color(355, 70, 95);
+	boxCols[2] = color(120, 50, 95);
+	boxCols[3] = color(0, 0, 92);
+	boxCols[4] = color(60, 90, 95);
+	boxCols[5] = color(0, 0, 100);
+
+	txtCol = color(0, 0, 0);
+	boxShaCol = color(0, 0, 0, 70);
+	txtShaCol = color(0, 0, 0, 30);
+
+	totalFrames = 30 * 12; // 12 seconds at 30fps
+
+	/***************************************** device orientation test **********/
+	createPermissionModal(); // hidden by default
+	let permissionGranted = false;
+	let nonios13device = false;
+	let permissionModal;
+	//	let rotationX = 0;
+	//	let rotationY = 0; // for device orientation event
+
+	if (typeof (DeviceOrientationEvent) !== 'undefined' && typeof (DeviceOrientationEvent.requestPermission) === 'function') {
+		DeviceOrientationEvent.requestPermission()
+			.catch(() => {
+				// show permission dialog only the first time
+				permissionModal = document.querySelector('.permission-modal-container');
+				permissionModal.style.display = 'block';
+
+				const cancelButton = document.querySelector('#button-cancel');
+				cancelButton.addEventListener('click', function () {
+					permissionModal.remove();
+				})
+				const allowButton = document.querySelector('#button-allow');
+				allowButton.addEventListener('click', onAskButtonClicked);
+
+				throw error; // keep the promise chain as rejected
+			})
+			.then(() => {
+				// this runs on subsequent visits
+				permissionGranted = true;
+			})
+	} else {
+		// it's up to you how to handle non ios 13 devices
+		nonios13device = true;
+		console.log('non iOS 13 device is being used.');
+	}
+
+	// will handle first time visiting to grant access
+	function onAskButtonClicked() {
+		permissionModal.remove();
+		DeviceOrientationEvent.requestPermission().then(response => {
+			if (response === 'granted') {
+				permissionGranted = true;
+			} else {
+				permissionGranted = false;
+			}
+		}).catch(console.error);
+	}
 }
 
-function disableScroll(){
-    document.body.addEventListener('touchmove', preventDefault, { passive: false });
+function draw() {
+	background(bgCol);
+	noStroke();
+
+
+	fill(50, 30, 40);
+	let rnum = 0;
+	for (let j = 0; j < h; j += h / 20) {
+		for (let i = 0; i < w; i += w / 10) {
+			if (rnum % 2 == 0) ellipse(i, j, 20, 20);
+			else ellipse(i + w / 20, j, 20, 20);
+		}
+		rnum++;
+	}
+
+	speed = frameCount / totalFrames * TWO_PI;
+
+	if (mouseIsPressed) {
+		tloc.x = mouseX;
+		tloc.y = mouseY;
+	} else {
+		// for mobile
+		tloc.x = w / 2 + constrain(map(rotationY, -30, 30, -w / 2, w / 2), -w / 2, w / 2);
+		tloc.y = h / 2 + constrain(map(rotationX, -30, 30, -h / 2, h / 2), -h / 2, h / 2);
+	}
+	loc.x = lerp(loc.x, tloc.x, .1);
+	loc.y = lerp(loc.y, tloc.y, .1);
+
+
+	ctx.shadowColor = txtShaCol;
+	ctx.shadowBlur = w * .02;
+	ctx.shadowOffsetX = (w / 2 - loc.x) * .5;
+	ctx.shadowOffsetY = (h / 2 - loc.y) * .5;
+	push();
+	translate(w / 2, h / 2);
+	fill(txtCol);
+	textCircle(h * .09, h * .09, w * .05);
+	pop();
+
+	const bw = w - (w * .125); // first box width
+	const bh = h - (w * .125); // first box height
+
+	for (let i = numShapes - 1; i >= 0; i--) {
+		const lx = lerp(w / 2, loc.x, i / numShapes);
+		const ly = lerp(h / 2, loc.y, i / numShapes);
+		let shp = shps[i];
+		shp.setContourRect(lx, ly, bw - (i * w * .13), bh - (i * h * .13));
+
+		ctx.shadowColor = boxShaCol;
+		ctx.shadowBlur = w * .1;
+		ctx.shadowOffsetX = 0;
+		ctx.shadowOffsetY = 0;
+		//		fill(boxCol);
+		fill(90 + i * 20, i * 20, 100 - i * 6);
+		//		fill(boxCols[i]);
+		//		if (i == 0) fill(txtCol);
+		shp.display();
+
+		ctx.shadowColor = txtShaCol;
+		ctx.shadowBlur = w * .02;
+		ctx.shadowOffsetX = (w / 2 - loc.x) * .35;
+		ctx.shadowOffsetY = (h / 2 - loc.y) * .35;
+		fill(txtCol);
+
+		push();
+		translate(w / 2, h / 2);
+		fill(txtCol);
+		if (i == 1) {
+			textCircle(h * .36, h * .36, w * .16); //.14
+		} else if (i == 3) {
+			textCircle(h * .2, h * .2, w * .1); // .1
+		}
+		pop();
+	}
+
+	ctx.shadowColor = 'transparent';
+
+	textSize(w * .033);
+	textAlign(CENTER, TOP);
+	textStyle(ITALIC);
+	fill(50, 20, 70);
+
+	const toffx = w * .013;
+	const toffy = w * .013;
+
+	push();
+	translate(w * .5, toffy);
+	text(str1, 0, 0);
+	pop();
+
+	push();
+	translate(w - toffx, h * .5);
+	rotate(PI / 2);
+	text(str2, 0, 0);
+	pop();
+
+	push();
+	translate(w * .5, h - toffy);
+	rotate(PI);
+	text(str1, 0, 0);
+	pop();
+
+	push();
+	translate(toffy, h * .5);
+	rotate(-PI / 2);
+	text(str2, 0, 0);
+	pop();
+
+	stroke(50, 20, 70);
+	line(w * .15, h * .032, w - w * .15, h * .032); // t
+	line(w * .952, h * .18, w * .952, h - h * .18); // r
+	line(w * .15, h - h * .032, w - w * .15, h - h * .032); // b
+	line(w - w * .952, h * .18, w - w * .952, h - h * .18); // l
+
+
+	/*
+	heart: ♥️ Unicode: U+2665 U+FE0F, 
+	club: ♣️ Unicode: U+2663 U+FE0F, 
+	spade: ♠️ Unicode: U+2660 U+FE0F, 
+	diamond: ♦️ Unicode: U+2666 U+FE0F, 
+	*/
+	textStyle(NORMAL);
+	textSize(w * .1);
+	textAlign(CENTER, CENTER);
+	//	fill(50, 80, 95);
+	fill(50, 50, 60);
+	text('\u2666', w * .06, h * .06); // diamond
+	//	fill(0, 90, 90);
+	text('\u2665', w - w * .06, h - h * .04); // heart
+	//	fill(110, 75, 90);
+	text('\u2663', w - w * .06, h * .06); // club
+	//	fill(230, 85, 90);
+	text('\u2660', w * .06, h - h * .04); // spade
+
+	//	if (frameCount % 15 == 0) console.log(frameRate());
 }
-function enableScroll(){
-    document.body.removeEventListener('touchmove', preventDefault);
+
+function textCircle(wi, hi, tsz) {
+	textAlign(CENTER, CENTER);
+	textStyle(ITALIC);
+	for (let i = 0; i < msg.length; i++) {
+		const x = .7 * cos(i * TWO_PI / msg.length - speed) * wi;
+		const y = 1.2 * sin(i * TWO_PI / msg.length - speed) * hi;
+		push();
+		translate(x, y);
+		const angle = atan2(y, x);
+		rotate(PI / 2 + angle);
+		textSize(tsz);
+		text(msg[(i + floor(tsz)) % msg.length], 0, 0);
+		pop();
+	}
+}
+
+function calcDimensionFromRatio(ratio) {
+	const curRatio = windowWidth / windowHeight;
+	let sz = createVector();
+	if (curRatio > ratio) { // wide
+		sz.y = windowHeight;
+		sz.x = sz.y * ratio;
+	} else { // tall
+		sz.x = windowWidth;
+		sz.y = sz.x * 1 / ratio;
+	}
+	return createVector(sz.x, sz.y);
+}
+
+function windowResized() {
+	const dim = calcDimensionFromRatio(ratio);
+	resizeCanvas(dim.x, dim.y);
+	canvas.position((windowWidth - canvas.width) / 2, (windowHeight - canvas.height) / 2);
+	w = width;
+	h = height;
+}
+
+class Compound {
+	constructor() {
+		this.ct = createVector(); // center
+		this.ctrW = 0;
+		this.ctrH = 0;
+	}
+
+	setContourRect(cx, cy, w, h) {
+		this.ct.x = cx;
+		this.ct.y = cy;
+		this.ctrW = w;
+		this.ctrH = h;
+	}
+
+	display() {
+		// for contour corner points
+		const offx = this.ctrW * .1;
+		const offy = this.ctrW * .1;
+
+		beginShape();
+		vertex(0, 0);
+		vertex(width, 0);
+		vertex(width, height);
+		vertex(0, height);
+
+		beginContour();
+		vertex(this.ct.x - this.ctrW / 2, this.ct.y);
+		bezierVertex(this.ct.x - this.ctrW / 2, this.ct.y + this.ctrH / 2 - offy, this.ct.x - this.ctrW / 2, this.ct.y + this.ctrH / 2 - offy, this.ct.x - this.ctrW / 2 + offx, this.ct.y + this.ctrH / 2 - offy);
+		bezierVertex(this.ct.x - this.ctrW / 2 + offx, this.ct.y + this.ctrH / 2, this.ct.x - this.ctrW / 2 + offx, this.ct.y + this.ctrH / 2, this.ct.x, this.ct.y + this.ctrH / 2);
+		bezierVertex(this.ct.x + this.ctrW / 2 - offx, this.ct.y + this.ctrH / 2, this.ct.x + this.ctrW / 2 - offx, this.ct.y + this.ctrH / 2, this.ct.x + this.ctrW / 2 - offx, this.ct.y + this.ctrH / 2 - offy);
+		bezierVertex(this.ct.x + this.ctrW / 2, this.ct.y + this.ctrH / 2 - offy, this.ct.x + this.ctrW / 2, this.ct.y + this.ctrH / 2 - offy, this.ct.x + this.ctrW / 2, this.ct.y);
+		bezierVertex(this.ct.x + this.ctrW / 2, this.ct.y - this.ctrH / 2 + offy, this.ct.x + this.ctrW / 2, this.ct.y - this.ctrH / 2 + offy, this.ct.x + this.ctrW / 2 - offx, this.ct.y - this.ctrH / 2 + offy);
+		bezierVertex(this.ct.x + this.ctrW / 2 - offx, this.ct.y - this.ctrH / 2, this.ct.x + this.ctrW / 2 - offx, this.ct.y - this.ctrH / 2, this.ct.x, this.ct.y - this.ctrH / 2);
+		bezierVertex(this.ct.x - this.ctrW / 2 + offx, this.ct.y - this.ctrH / 2, this.ct.x - this.ctrW / 2 + offx, this.ct.y - this.ctrH / 2, this.ct.x - this.ctrW / 2 + offx, this.ct.y - this.ctrH / 2 + offy);
+		bezierVertex(this.ct.x - this.ctrW / 2, this.ct.y - this.ctrH / 2 + offy, this.ct.x - this.ctrW / 2, this.ct.y - this.ctrH / 2 + offy, this.ct.x - this.ctrW / 2, this.ct.y);
+		endContour();
+
+		endShape();
+	}
 }
